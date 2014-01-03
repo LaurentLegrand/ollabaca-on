5dd78a39183dc414fd9da9d15c3a6c5d222fcdcc
Laurent Legrand <laurent-git@ollabaca.org>
2014-01-03 17:48:47 +0100

    replace Activator by UmlSitePlugin

---
+++ org.ollabaca.on.uml/src/org/ollabaca/on/uml/UmlSitePlugin.java
@@ -0,0 +1,12 @@
+package org.ollabaca.on.uml;
+
+import org.ollabaca.on.site.SitePlugin;
+
+public class UmlSitePlugin implements SitePlugin {
+
+	@Override
+	public void activate() {
+		Notation_Element.register();		
+	}
+
+}
